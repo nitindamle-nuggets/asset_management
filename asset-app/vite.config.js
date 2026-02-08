a2429f63a8536115923c3d05d07dbe6f76903a0d
nitindamle-nuggets <243072838+nitindamle-nuggets@users.noreply.github.com>
2026-02-08 17:57:58 +0530

    Refactor Vite config to use defineConfig

--- asset-app/vite.config.js
+++ asset-app/vite.config.js
@@ -2,19 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-export default {
+export default defineConfig({
+  plugins: [react()],
   server: {
     host: "0.0.0.0",
     port: 5173,
-    allowedHosts: ["restrictively-unfleeced-blanche.ngrok-free.dev"]
+    allowedHosts: "all"
   }
-}
-// export default {
-//   server: {
-//     host: "0.0.0.0",
-//     port: 5173
-//   }
-// }
+})
